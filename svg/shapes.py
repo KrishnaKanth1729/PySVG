fbb3d14c8bc4407519f4bf6f5d09ccf064ca014a
KrishnaKanth1729 <rkrishnakanth1729@gmail.com>
2021-11-05 21:21:42 +0530

    shapes

--- svg/shapes.py
+++ svg/shapes.py
@@ -1,6 +1,17 @@
 from style import Style
 from typing import List
 
+class Rectangle:
+    def __init__(self, width: int, height: int, x: int = 0, y: int = 0, style: Style = Style({"fill": "black"})):
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+        self.style = style
+
+    def svg_content(self) -> str:
+        content = f'<rect width="{self.width}" height="{self.height}" x={self.x} y={self.y} style={self.style.svg()} />'
+        return content
 
 class Circle:
     def __init__(self, cx: int, cy: int, r: int, style: Style, stroke_width: int = None, fill: str = None):
@@ -30,14 +41,4 @@ class Polygon:
         return f'<polygon points="{" ".join([point.string() for point in self.points])}" style={self.style.svg()} />'
 
 
-class Rectangle:
-    def __init__(self, width: int, height: int, x: int = 0, y: int = 0, style: Style = Style({"fill": "black"})):
-        self.width = width
-        self.height = height
-        self.x = x
-        self.y = y
-        self.style = style
 
-    def svg_content(self) -> str:
-        content = f'<rect width="{self.width}" height="{self.height}" x={self.x} y={self.y} style={self.style.svg()} />'
-        return content
